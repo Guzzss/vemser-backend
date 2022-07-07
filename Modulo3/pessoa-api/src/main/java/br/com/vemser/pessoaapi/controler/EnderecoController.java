baf1f169daf58788f419af4c62237a8475eba600
Gustavo Teichmann <gustavo.teichmann@dbccompany.com.br>
2022-07-06 22:36:08 -0300

    Homework-3 De Spring Boot

--- Modulo3/pessoa-api/src/main/java/br/com/vemser/pessoaapi/controler/EnderecoController.java
+++ Modulo3/pessoa-api/src/main/java/br/com/vemser/pessoaapi/controler/EnderecoController.java
@@ -14,34 +14,34 @@ public class EnderecoController {
     @Autowired
     private EnderecoService enderecoService;
 
-    @GetMapping("/endereco") // localhost:8090/endereco
+    @GetMapping// localhost:8090/endereco
     public List<Endereco> list() {
         return enderecoService.list();
     }
 
-    @GetMapping("/endereco/{idEndereco}") // localhost:8090/endereco/1
+    @GetMapping("/{idEndereco}") // localhost:8090/endereco/1
     public List<Endereco> listByIdEndereco(@PathVariable("idEndereco") Integer id) {
         return enderecoService.listEnderecoByIdEndereco(id);
     }
 
-    @GetMapping("/endereco/{idPessoa}/pessoa") // localhost:8090/endereco/1/pessoa
+    @GetMapping("/{idPessoa}/pessoa") // localhost:8090/endereco/1/pessoa
     public List<Endereco> listByIdPessoa(@PathVariable("idPessoa") Integer id) {
         return enderecoService.listEnderecoByIdPessoa(id);
     }
 
-    @PostMapping("/endereco/{idPessoa}") // localhost:8090/endereco
+    @PostMapping("/{idPessoa}") // localhost:8090/endereco
     public Endereco create(@PathVariable("idPessoa") Integer id,
                            @RequestBody Endereco endereco) {
         return enderecoService.create(endereco, id);
     }
 
-    @PutMapping("/endereco/{idEndereco}") // localhost:8090/endereco/1
+    @PutMapping("/{idEndereco}") // localhost:8090/endereco/1
     public Endereco update(@PathVariable("idEndereco") Integer id,
                            @RequestBody Endereco enderecoAtualizar) throws Exception {
         return enderecoService.update(id, enderecoAtualizar);
     }
 
-    @DeleteMapping("/endereco/{idEndereco}") // localhost:8090/endereco/10
+    @DeleteMapping("/{idEndereco}") // localhost:8090/endereco/10
     public void delete(@PathVariable("idEndereco") Integer id) throws Exception {
         enderecoService.delete(id);
     }
